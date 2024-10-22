148b5747b1857e3f8e7f38b3e34b1e198ddff86d
Rebecca Bettencourt <beckiergb@gmail.com>
2024-10-22 13:28:53 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -17,7 +17,7 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0x80
+    dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
@@ -25,62 +25,34 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    async def reg_write(a, d):
+        dut.ui_in.value = ((d >> 4) & 0x70) | a
+        dut.uio_in.value = d & 0xFF
+        await ClockCycles(dut.clk, 1)
+        dut.ui_in.value = 0x80 | ((d >> 4) & 0x70) | a
+        dut.uio_in.value = d & 0xFF
+        await ClockCycles(dut.clk, 1)
+        dut.ui_in.value = ((d >> 4) & 0x70) | a
+        dut.uio_in.value = d & 0xFF
+        await ClockCycles(dut.clk, 1)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
+    await reg_write(0, 0)
     assert dut.uo_out.value == 0b01110111
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
-    dut.ui_in.value = 0x83
-    dut.uio_in.value = 0x00
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x03
-    dut.uio_in.value = 0x00
-    await ClockCycles(dut.clk, 1)
-
-    dut.ui_in.value = 0x8C
-    dut.uio_in.value = 0x00
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x0C
-    dut.uio_in.value = 0x00
-    await ClockCycles(dut.clk, 1)
+    await reg_write(3, 0)
+    await reg_write(12, 0x00)
     assert dut.uo_out.value == 0b00000000
 
-    dut.ui_in.value = 0x8C
-    dut.uio_in.value = 0x03
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x0C
-    dut.uio_in.value = 0x03
-    await ClockCycles(dut.clk, 1)
+    await reg_write(12, 0x03)
     assert dut.uo_out.value == 0b01000100
 
-    dut.ui_in.value = 0x8C
-    dut.uio_in.value = 0x0C
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x0C
-    dut.uio_in.value = 0x0C
-    await ClockCycles(dut.clk, 1)
+    await reg_write(12, 0x0C)
     assert dut.uo_out.value == 0b00100010
 
-    dut.ui_in.value = 0x8C
-    dut.uio_in.value = 0x30
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x0C
-    dut.uio_in.value = 0x30
-    await ClockCycles(dut.clk, 1)
+    await reg_write(12, 0x30)
     assert dut.uo_out.value == 0b00010001
 
-    dut.ui_in.value = 0x8C
-    dut.uio_in.value = 0x3F
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value = 0x0C
-    dut.uio_in.value = 0x3F
-    await ClockCycles(dut.clk, 1)
+    await reg_write(12, 0x3F)
     assert dut.uo_out.value == 0b01110111

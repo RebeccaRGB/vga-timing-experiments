9f2628d47295b0220a57979636ef0e62b63454b5
RebeccaRGB <beckiejo@gmail.com>
2025-08-24 02:33:00 -0700

    More correct test?

--- test/test.py
+++ test/test.py
@@ -106,41 +106,45 @@ async def test_project(dut):
     frame.save(f"output/frame0.png")
 
     dut.ui_in.value = 0x83
+    await skip_frame(1)
+    dut.ui_in.value = 0
+
     frame = await capture_frame(1)
     frame.save(f"output/frame1.png")
 
-    dut.ui_in.value = 0
-    await skip_frame(2)
-
     dut.ui_in.value = 0x8C
     dut.uio_in.value = 0x03
+    await skip_frame(2)
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
     frame = await capture_frame(2)
     frame.save(f"output/frame2.png")
 
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    await skip_frame(3)
-
     dut.ui_in.value = 0x8C
     dut.uio_in.value = 0x0C
+    await skip_frame(3)
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
     frame = await capture_frame(3)
     frame.save(f"output/frame3.png")
 
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    await skip_frame(4)
-
     dut.ui_in.value = 0x8C
     dut.uio_in.value = 0x30
+    await skip_frame(4)
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
     frame = await capture_frame(4)
     frame.save(f"output/frame4.png")
 
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    await skip_frame(5)
-
     dut.ui_in.value = 0x8C
     dut.uio_in.value = 0x3F
+    await skip_frame(5)
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+
     frame = await capture_frame(5)
     frame.save(f"output/frame5.png")
 
